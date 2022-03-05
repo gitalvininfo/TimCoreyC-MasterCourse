42708d3e67b88fb21d9f562df95d4a46b1fd31c8
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-05 16:28:49 +0800

    done with timeonly

--- CSharpMasterCourse/Program.cs
+++ CSharpMasterCourse/Program.cs
@@ -1,16 +1,16 @@
 ﻿
 
-DateTime today = DateTime.Now;
 
-// 
-DateOnly birthday = DateOnly.Parse("6/11/1998");
-
-Console.WriteLine(birthday.ToString("MMMM dd, yyyy"));
+TimeOnly opensAt = TimeOnly.Parse("8:00 AM");
 
 
-Console.WriteLine("Today full: " + today); // 05/03/2022 4:21:00 pm
-Console.WriteLine("Today just date: " + today.Date); // 05/03/2022 12:00:00 am
-Console.WriteLine("Birthday full: " + birthday); // 06/11/1998
+TimeOnly rightNow = TimeOnly.FromDateTime(DateTime.Now);
+
+Console.WriteLine(opensAt);
+
+Console.WriteLine(rightNow);
+
+
 
 Console.Beep();
 Console.ReadKey();
