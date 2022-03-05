9e64a6e402dd37776c4a4062fcc2124abfe478dc
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-05 17:04:38 +0800

    done with casting

--- CSharpMasterCourse/Program.cs
+++ CSharpMasterCourse/Program.cs
@@ -1,15 +1,73 @@
 ﻿
 
 
-TimeOnly opensAt = TimeOnly.Parse("8:00 AM");
+Console.Write("What is your age: ");
+string ageText = Console.ReadLine();
+
+bool isValidInt = int.TryParse(ageText, out int age);
+
+Console.WriteLine($"this is valid: {isValidInt}. The number was {age}.");
+
+Console.WriteLine("Your age is " + age);
 
 
-TimeOnly rightNow = TimeOnly.FromDateTime(DateTime.Now);
+double testDouble = age;
 
-Console.WriteLine(opensAt);
+decimal testDecimal = (decimal)testDouble;
 
-Console.WriteLine(rightNow);
 
+
+Console.WriteLine(testDecimal);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//bool isContinue = true;
+
+
+//while (isContinue)
+//{
+//    try
+//    {
+//        Console.Write("What is your age:  ");
+//        int age = Convert.ToInt32(Console.ReadLine());
+
+//        isContinue = true;
+
+//        age += 15;
+
+//        Console.WriteLine($"Your age plus 15 is {age}");
+
+//        Console.WriteLine("Do you want to continue? Y/N");
+
+//        if(Console.ReadLine() == "Y") 
+//        {
+//            isContinue = true;
+//        }   else
+//        {
+//            isContinue = false;
+//        }
+//    } catch (FormatException e)
+//    {
+//        Console.WriteLine("Enter only numbers pls.");
+//        continue;
+//    }
+//}
 
 
 Console.Beep();
