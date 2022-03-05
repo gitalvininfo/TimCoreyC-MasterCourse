34599faf989eb7c1c5c257870536030111b18aac
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-05 16:04:46 +0800

    done with date time

--- CSharpMasterCourse/Program.cs
+++ CSharpMasterCourse/Program.cs
@@ -1,11 +1,14 @@
 ﻿
 
-string personsName = "Alvin Yanson";
-int age = 24;
-bool isAlive = true;
-string phoneNumber = "433-4142";
+using System.Globalization;
 
+DateTime today = DateTime.Now;
 
+//DateTime birthday = DateTime.Parse("08/12/1997");
+
+DateTime birthday = DateTime.ParseExact("08/12/1997", "d/M/yyyy", CultureInfo.InvariantCulture);
+
+Console.WriteLine(birthday);
 
 
 
