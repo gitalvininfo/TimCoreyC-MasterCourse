6aae92c6cbfefd2c66df37ea95d1f7879710c97a
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-06 02:18:38 +0800

    done with homework gg ez

--- CSharpMasterCourse/Program.cs
+++ CSharpMasterCourse/Program.cs
@@ -1,49 +1,48 @@
 ﻿
-
-bool isValidAge;
-int age;
-//int testNumber = 0;
-
-//do
-//{
-//    Console.WriteLine(testNumber);
-//    testNumber++;
-//} while (testNumber != 10);
-
+string response = "";
+bool isValidResponse = false;
 
 do
 {
-    Console.Write("Enter your age: ");
-    string ageText = Console.ReadLine();
 
-    isValidAge = int.TryParse(ageText, out age);
+    Console.Write("Enter your name: ");
+    string name = Console.ReadLine();
 
-    if (!isValidAge)
+    if (name.ToLower() == "tim")
     {
-        Console.WriteLine("That was an invalid age");
+        Console.WriteLine($"Hello Professor {name}.");
+    }
+    else
+    {
+        Console.WriteLine("Hello Student.");
     }
 
-} while (!isValidAge);
 
-Console.WriteLine("You are valid age");
+    do
+    {
+        Console.Write("Continue? Y/N: ");
+        response = Console.ReadLine();
+
+        response = response.ToLower();
+
+        if (response.ToLower() != "y" && response.ToLower() != "n")
+        {
+            isValidResponse = false;
+        }
+        else
+        {
+            isValidResponse = true;
+        }
+
+        if(response.ToLower() == "n")
+        {
+            Console.WriteLine("Goodbye :P");
+        }
+
+    } while (!isValidResponse);
 
 
-
-
-// runs the code at least once.
-//do
-//{
-
-//} while(true)
-
-
-// runs the code 0 or more times
-//while(true)
-//{
-
-//}
-
-
+} while (response == "y");
 
 
 Console.Beep();
