466191216803ec7275bc492a2cef7f347ff77231
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-06 01:09:12 +0800

    gg ez mini project

--- CSharpMasterCourse/Program.cs
+++ CSharpMasterCourse/Program.cs
@@ -1,44 +1,61 @@
-﻿Console.Write("Enter your name user: ");
-string name = Console.ReadLine();
+﻿
+
+bool isContinue = true;
 
 
-if(name.ToLower() == "tim")
+while (isContinue)
 {
-    Console.WriteLine("Hello Professor!");
-}   else
-{
-    Console.WriteLine("Hello Student!");
+
+    Console.Write("Enter your name: ");
+    string name = Console.ReadLine();
+
+    Console.Write("Enter your age: ");
+    int age = Convert.ToInt32(Console.ReadLine());
+
+    int waitingYears = 21 - age;
+
+    bool isProfessor = name.ToLower() == "bob" || name.ToLower() == "sue" || name.ToLower() == "tim";
+
+    if (isProfessor)
+    {
+        Console.WriteLine("Hello Professor!");
+    }
+
+    else
+    {
+        Console.WriteLine("Hello Student!");
+        if (age < 21)
+        {
+            Console.WriteLine($"You must wait {waitingYears} years to start class");
+        }
+        else
+        {
+            Console.WriteLine("Go to school user...");
+        }
+    }
+
+    bool isValidResponse = false;
+
+    while (!isValidResponse){
+        Console.WriteLine("Do you want to continue? Y/N");
+        string response = Console.ReadLine();
+
+        if (response.ToLower() == "y")
+        {
+            isValidResponse = true;
+            isContinue = true;
+        }
+        else if (response.ToLower() == "n")
+        {
+            isValidResponse = true;
+            isContinue = false;
+            Console.WriteLine("Thanks for playing...");
+        }   else
+        {
+            isValidResponse = false;
+        }
+    }
 }
-
-
-
-
-
-
-//switch(name.ToLower())
-//{
-//    case "tim":
-//        Console.WriteLine("Hello Professor!");
-//        break;
-
-//    default:
-//        Console.WriteLine("Hello Student!");
-//        break;
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 Console.Beep();
