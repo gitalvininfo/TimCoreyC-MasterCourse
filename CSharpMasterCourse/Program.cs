2dbe17f1a7122f8f12e29120dd95a02acb22e4ed
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-05 13:02:40 +0800

    done with string literals and interpolations gg ez

--- CSharpMasterCourse/Program.cs
+++ CSharpMasterCourse/Program.cs
@@ -1,4 +1,23 @@
 ﻿
+String firstName = String.Empty;
+String lastName = String.Empty;
+String filePath = String.Empty;
 
-String firstname = "Alvin";
-Console.WriteLine(firstname);
+firstName = "Alvin";
+lastName = "Yanson";
+// just like \n the \ identifies the next character as special
+// since it is also a slash, then it will consider as slash
+//filePath = "C:\\Temp\\Demo";
+
+// the @ treats the string as string literal characters; gg ez;
+filePath = @"C:\Temp\Demo";
+
+String testString = $@"The file for {firstName} is at {filePath}";
+
+
+Console.WriteLine($"{firstName} {lastName}");
+Console.WriteLine($"{filePath}");
+Console.WriteLine(testString);
+
+Console.Beep();
+Console.ReadKey();
