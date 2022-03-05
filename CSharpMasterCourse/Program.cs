88bb1877eb9a502690a8e0ec03f3c9820aa94d5b
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-06 02:44:18 +0800

    done with arrays

--- CSharpMasterCourse/Program.cs
+++ CSharpMasterCourse/Program.cs
@@ -1,48 +1,38 @@
 ﻿
-string response = "";
-bool isValidResponse = false;
+//string[] firstnames = new string[5];
 
-do
+//firstnames[0] = "alvin1";
+//firstnames[1] = "alvin2";
+//firstnames[2] = "alvin3";
+//firstnames[3] = "alvin4";
+//firstnames[4] = "alvin5";
+//firstnames[5] = "alvin";
+
+
+//foreach (string firstname in firstnames) {
+//    Console.WriteLine(firstname);
+//}
+
+
+string data = "Tim,Sue,Bob,Jane,Frank";
+
+string[] firstNames = data.Split(",");
+
+foreach (string firstname in firstNames)
 {
+    Console.Write($"{firstname}, ");
 
-    Console.Write("Enter your name: ");
-    string name = Console.ReadLine();
+}
 
-    if (name.ToLower() == "tim")
-    {
-        Console.WriteLine($"Hello Professor {name}.");
-    }
-    else
-    {
-        Console.WriteLine("Hello Student.");
-    }
+Console.WriteLine();
 
+string[] lastNames = new string[] { "Corey", "Smith", "Jones" };
 
-    do
-    {
-        Console.Write("Continue? Y/N: ");
-        response = Console.ReadLine();
+foreach (string lastname in lastNames)
+{
+    Console.Write($"{lastname}, ");
+}
 
-        response = response.ToLower();
-
-        if (response.ToLower() != "y" && response.ToLower() != "n")
-        {
-            isValidResponse = false;
-        }
-        else
-        {
-            isValidResponse = true;
-        }
-
-        if(response.ToLower() == "n")
-        {
-            Console.WriteLine("Goodbye :P");
-        }
-
-    } while (!isValidResponse);
-
-
-} while (response == "y");
 
 
 Console.Beep();
