c1c4c6a873e60868dd46337a3e1aa0703e692212
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-07 00:42:24 +0800

    done in homework gg ez

---
+++ CSharpMasterCourse/Homework.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMasterCourse
+{
+    public static class Homework
+    {
+
+        public static void Welcome()
+        {
+            Console.WriteLine("Welcome User!");
+        }
+
+        public static string GetUserName()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+        public static void Hello(string name)
+        {
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
--- CSharpMasterCourse/Program.cs
+++ CSharpMasterCourse/Program.cs
@@ -3,42 +3,9 @@
 using CSharpMasterCourse;
 
 
-// ignore lastName
-//(string firstName, _);
-//(string firstName, string lastName);
-var name = ConsoleMessages.GetFullName();
-
-Console.WriteLine($"Firstname is {name.firstName}, Lastname is {name.lastName}");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//string name = ConsoleMessages.GetUsersName();
-
-//ConsoleMessages.SayHi(name);
-//ConsoleMessages.SayGoodbye();
-
-//double result = MathShortcuts.Add(3, 5);
-//Console.WriteLine(result);
-
-//double[] vals = { 2, 5, 6, 21, 52, 98 };
-
-//MathShortcuts.AddAll(vals);
+Homework.Welcome();
+string name = Homework.GetUserName();
+Homework.Hello(name);
 
 Console.Beep();
 Console.ReadKey();
