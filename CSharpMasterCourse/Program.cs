cf90e2cf871ace7f15db7d9d05b518733df90447
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-06 21:16:43 +0800

    done with foreach loop

--- CSharpMasterCourse/Program.cs
+++ CSharpMasterCourse/Program.cs
@@ -1,13 +1,54 @@
 ﻿
+List<string> names = new();
+String name;
+String response = "";
+bool isValidResponse = false;
 
-string data = "Tim,Corey,Jane,Sue";
-List<string> names = data.Split(",").ToList();
 
-
-foreach(string name in names)
+do
 {
-    Console.WriteLine(name);
-}
+    Console.Write("Enter your name: ");
+    name = Console.ReadLine();
+    names.Add(name);
+
+
+    do
+    {
+        Console.Write("Name added. Continue Y/N: ");
+        response = Console.ReadLine();
+        response = response.ToLower();
+
+
+        if (response == "y" || response == "n")
+        {
+            isValidResponse = true;
+
+        }
+        else
+        {
+            isValidResponse = false;
+
+        }
+
+        if (response == "n")
+        {
+            foreach (string uname in names)
+            {
+                Console.WriteLine($"Hello {uname}");
+            }
+        }
+
+
+    } while (!isValidResponse);
+
+
+} while (response == "y");
+
+
+
+//if == it must be response == "y" || response == "n"
+
+//if && it must be response != "y" && response != "n"
 
 Console.Beep();
 Console.ReadKey();
