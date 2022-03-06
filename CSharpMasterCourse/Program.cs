96e9affb90df1f24038d2630aa08309c23ff83dc
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-06 20:27:34 +0800

    done with homework dictionary

--- CSharpMasterCourse/Program.cs
+++ CSharpMasterCourse/Program.cs
@@ -1,22 +1,36 @@
 ﻿
 
-Dictionary<string, string> lookup = new Dictionary<string, string>();
-
-lookup["animal"] = "not a human";
-lookup["fish"] = "not a human that swims";
-lookup["human"] = "Us";
-Console.WriteLine($"the definition of fish is {lookup["fish"]}");
-
-
-
-
 Dictionary<int, string> employees = new Dictionary<int, string>();
 
-employees[95] = "Tim Corey";
-employees[28] = "Sue Storm";
+
+employees[1] = "alvin1";
+employees[2] = "alvin2";
+employees[3] = "alvin3";
+employees[4] = "alvin4";
+employees[5] = "alvin5";
+
+Console.Write("Please choose employee 1-5: ");
+string idString = Console.ReadLine();
+
+bool isValidID = int.TryParse(idString, out int id);
+
+if(isValidID)
+{
+    if (id <= 5)
+    {
+        Console.WriteLine($"The employee is {employees[id]}.");
+    }
+    else
+    {
+        Console.WriteLine("That employee does not exist.");
+    }
+}
+else
+{
+    Console.WriteLine("That is not a number");
+}
 
 
-Console.WriteLine($"the employee with id number 28 is {employees[28]}");
 
 
 Console.Beep();
