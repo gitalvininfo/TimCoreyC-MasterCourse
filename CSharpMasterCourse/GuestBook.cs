eddb5619b85cbab5fe1fbcfa1ca002298869a4dc
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-07 23:26:25 +0800

    gg ez homework guestbook

---
+++ CSharpMasterCourse/GuestBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMasterCourse
+{
+    public static class GuestBook
+    {
+        public static string GetUserName()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+        public static int GetNumberOfPeople()
+        {
+            Console.Write("How many people you have: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            return number;
+        }
+    }
+}
--- CSharpMasterCourse/Program.cs
+++ CSharpMasterCourse/Program.cs
@@ -2,10 +2,55 @@
 
 using CSharpMasterCourse;
 
+List<string> users = new();
+List<int> guests = new();
 
-Homework.Welcome();
-string name = Homework.GetUserName();
-Homework.Hello(name);
+bool isContinue = true;
+string response = "";
+bool isValidResponse = false;
+
+do
+{
+    string name = GuestBook.GetUserName();
+    users.Add(name);
+
+    int num = GuestBook.GetNumberOfPeople();
+    guests.Add(num);
+
+    do
+    {
+        Console.Write("Do you want to continue. Y/N: ");
+        response = Console.ReadLine();
+
+        if (response.ToLower() != "y" && response.ToLower() != "n")
+        {
+            isValidResponse = false;
+        }   else
+        {
+            isValidResponse = true;
+        }
+
+        if(response.ToLower() == "n")
+        {
+            Console.WriteLine("The users are the following: ");
+            foreach (string user in users)
+            {
+                Console.Write(user + ", ");
+            }
+
+            int total = 0;
+            foreach(int guest in guests)
+            {
+                total += guest;
+            }
+            Console.WriteLine("The total number of guests is: " + total);
+
+
+        }
+
+    } while (!isValidResponse);
+
+} while (response.ToLower() == "y");
 
 Console.Beep();
 Console.ReadKey();
