cf10238d6bb7e6d9dd77f35eeff3552b62ced60f
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-10 00:01:52 +0800

    done with advanced exceptions

--- CSharpMasterCourse/Program.cs
+++ CSharpMasterCourse/Program.cs
@@ -13,31 +13,71 @@ namespace CSharpMasterCourse
 
         static void Main(string[] args)
         {
-
+            string name = "";
             try
             {
-                BadCall();
-            } catch(Exception e)
-            {
-                Console.WriteLine($"caller > {e}");
+                DifferentMethod();
+
+                //Console.Write("What is your name? ");
+                //name = Console.ReadLine();
+                //ComplexMethod(name);
+                //SimpleMethod();
             }
-                     
+            /* specific */
+            catch(InvalidOperationException ex)
+            {
+                Console.WriteLine($"You should not be calling the SimpleMethod.");
+            }
+
+            catch(NotImplementedException)
+            {
+                Console.WriteLine("You forgot to finish your code!");
+            }
+            
+            /* general */
+            catch (Exception ex) when (name.ToLower() == "tim")
+            {
+                Console.WriteLine($"You used Tim's name, didn't you?");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex}");
+                //Console.WriteLine($"{ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("I always run");
+            }
+
 
             Console.Beep();
             Console.ReadKey();
         }
 
-        public static void BadCall()
+        private static void ComplexMethod(string name)
         {
-            for (int i = 1; i <= 5; i++)
+
+            if(name.ToLower() == "tim")
             {
-                Console.WriteLine(i);
-                if (i == 5)
-                {
-                    throw new Exception("Tim corey said I will throw");
-                }
+                throw new InsufficientMemoryException("Tim is too tall for this method.");
+            }   else
+            {
+                throw new ArgumentException("This person isn't Tim.");
             }
+            //throw new NotImplementedException();
         }
+
+        private static void SimpleMethod()
+        {
+            throw new InvalidOperationException("You should not be calling the SimpleMethod.");
+        }
+
+        private static void DifferentMethod()
+        {
+            Console.WriteLine("This is the different method working properly.");
+            //throw new NotImplementedException();
+        }
+
 
     }
 }
