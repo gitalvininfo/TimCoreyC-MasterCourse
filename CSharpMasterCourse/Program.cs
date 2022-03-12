c75b61736685915982ad56136276579c7e992016
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-13 02:19:13 +0800

    gg ez oop1

--- CSharpMasterCourse/Program.cs
+++ CSharpMasterCourse/Program.cs
@@ -1,10 +1,10 @@
 ﻿
+using GuestBookingLibrary.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using GuestBookLibrary.Models;
 
 // Capture the information about each guest
 // Info to capture: Firstname, lastname, message to the host
@@ -17,59 +17,43 @@ namespace CSharpMasterCourse
     {
         private static List<GuestModel> guests = new List<GuestModel>();
 
+
         static void Main(string[] args)
         {
 
-            GetGuestInformation();
+            string moreGuestToCome = "";
 
-            PrintGuestInformation();
+            do
+            {
+                GuestModel guest = new GuestModel();
+                Console.Write("What is your first name: ");
+                guest.FirstName = Console.ReadLine();
+
+                Console.Write("What is your last name: ");
+                guest.LastName = Console.ReadLine();
+
+                Console.Write("What is your message to the host: ");
+                guest.MessageToHost = Console.ReadLine();
 
 
+                Console.Write("Are more guest coming (yes/no): ");
+                moreGuestToCome = Console.ReadLine();
+
+                guests.Add(guest);
+
+                Console.Clear();
+            } while (moreGuestToCome.ToLower() == "yes");
+
+
+            foreach(GuestModel guest in guests)
+            {
+                Console.WriteLine(guest.GetGuestInfo);
+            }
 
             Console.Beep();
             Console.ReadLine();
         }
 
-        private static void PrintGuestInformation()
-        {
-            foreach (GuestModel guest in guests)
-            {
-                Console.WriteLine(guest.GuestInfo);
-            }
-        }
-
-        private static void GetGuestInformation()
-        {
-            string moreGuestComing = "";
-
-
-            do
-            {
-                GuestModel guest = new GuestModel();
-
-                guest.FirstName = GetInfoFromConsole("What is your first name: ");
-                guest.LastName = GetInfoFromConsole("What is your last name: ");
-                guest.MessageToHost = GetInfoFromConsole("What message would you like to tell the host: ");
-                moreGuestComing = GetInfoFromConsole("Are more guests coming (yes/no): ");
-
-
-                guests.Add(guest);
-
-
-                Console.Clear();
-            } while (moreGuestComing.ToLower() == "yes");
-
-        }
-
-        private static string GetInfoFromConsole(string message)
-        {
-            string output = "";
-
-            Console.Write(message);
-            output = Console.ReadLine();
-
-            return output;
-        }
 
     }
 }
---
+++ GuestBookingLibrary/Models/GuestModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBookingLibrary.Models
+{
+    public class GuestModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MessageToHost { get; set; }
+
+
+        public string GetGuestInfo
+        {
+            get
+            {
+                return $"{FirstName} {LastName}: {MessageToHost}";
+            }
+        }
+    }
+}
