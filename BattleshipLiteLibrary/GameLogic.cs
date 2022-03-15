0d697e4d1b90b4a41a10f181336fdbf5d9c2914d
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-16 00:28:11 +0800

    done in logic

--- BattleshipLiteLibrary/GameLogic.cs
+++ BattleshipLiteLibrary/GameLogic.cs
@@ -39,9 +39,19 @@ namespace BattleshipLiteLibrary
 
         }
 
-        public static bool PlayerStillActive(PlayerInfoModel opponent)
+        public static bool PlayerStillActive(PlayerInfoModel player)
         {
-            throw new NotImplementedException();
+            bool isActive = false;
+            /* check for each ship in player ship location is there is left not sunk */
+            foreach(var ship in player.ShipLocations) 
+            {
+                if(ship.Status != GridSpotStatus.Sunk)
+                {
+                    isActive = true;
+                }
+            }
+
+            return isActive;
         }
 
         private static void AddGridSpot(PlayerInfoModel model, string letter, int number)
@@ -56,34 +66,147 @@ namespace BattleshipLiteLibrary
             model.ShotGrid.Add(spot);
         }
 
-        public static int GetShotCount(PlayerInfoModel winner)
+        public static int GetShotCount(PlayerInfoModel player)
         {
-            throw new NotImplementedException();
+            int shotCount = 0;
+
+            foreach(var shot in player.ShotGrid)
+            {
+                /* count all the status except empty because that is not touch, hence that was not fired at. */
+                if(shot.Status != GridSpotStatus.Empty)
+                {
+                    shotCount += 1;
+                }
+            }
+
+            return shotCount;
         }
 
         public static bool PlaceShip(PlayerInfoModel model, string location)
         {
-            throw new NotImplementedException();
+            bool output = false;
+            (string row, int column) = SplitShotsIntoRowAndColumn(location);
+
+            bool isValidLocation = ValidateGridLocation(model, row, column);
+            bool isSpotOpen = ValidateShipLocation(model, row, column);
+
+            /* before placing the ship, check if it is valid and if it is already taken */
+            if(isValidLocation && isSpotOpen)
+            {
+                model.ShipLocations.Add(new GridSpotModel
+                {
+                    SpotLetter = row.ToUpper(),
+                    SpotNumber = column,
+                    Status = GridSpotStatus.Ship
+                });
+
+                output = true;
+            }
+
+            return output;
         }
 
-        public static (string rows, int column) SplitShotsIntoRowAndColumn(string shot)
+        private static bool ValidateShipLocation(PlayerInfoModel model, string row, int column)
         {
-            throw new NotImplementedException();
+            bool isValidLocation = true;
+
+            foreach(var ship in model.ShipLocations)
+            {
+                /* check for each row and column if it is already taken */
+                if(ship.SpotLetter == row.ToUpper() && ship.SpotNumber == column)
+                {
+                    isValidLocation = false;
+                }
+            }
+
+            return isValidLocation;
         }
 
-        public static bool ValidateShot(object activePlater, string row, int column)
+        private static bool ValidateGridLocation(PlayerInfoModel model, string row, int column)
         {
-            throw new NotImplementedException();
+            bool isValidLocation = false;
+
+            foreach (var ship in model.ShotGrid)
+            {
+                /* check for each row and column if it is valid like Z3 is invalid */
+                if (ship.SpotLetter == row.ToUpper() && ship.SpotNumber == column)
+                {
+                    isValidLocation = true;
+                }
+            }
+
+            return isValidLocation;
+        }
+
+        public static (string row, int column) SplitShotsIntoRowAndColumn(string shot)
+        {
+            string row = "";
+            int column = 0;
+
+            //if(shot.Length != 2)
+            //{
+            //    throw new ArgumentException("This was an invalid shot type", "shot");
+            //}
+
+            char[] shotArray = shot.ToArray();
+
+            row = shotArray[0].ToString();
+            column = int.Parse(shotArray[1].ToString());
+
+            return (row, column);    
+        }
+
+        public static bool ValidateShot(PlayerInfoModel player, string row, int column)
+        {
+            bool isValidShot = false;
+
+            foreach (var gridSpot in player.ShipLocations)
+            {
+                /* check for each row and column if it is already taken */
+                if (gridSpot.SpotLetter == row.ToUpper() && gridSpot.SpotNumber == column)
+                {
+                    if(gridSpot.Status == GridSpotStatus.Empty)
+                    {
+                        isValidShot = true;
+                    }
+                }
+            }
+
+            return isValidShot;
         }
 
         public static bool IdentifyShotResult(PlayerInfoModel opponent, string row, int column)
         {
-            throw new NotImplementedException();
+            bool isAHit = true;
+
+            foreach (var ship in opponent.ShipLocations)
+            {
+                /* check for each row and column if it is already taken */
+                if (ship.SpotLetter == row.ToUpper() && ship.SpotNumber == column)
+                {
+                    isAHit = false;
+                }
+            }
+
+            return isAHit;
         }
 
-        public static void MarkShotResult(PlayerInfoModel activePlayer, string row, int column, bool isAHit)
+        public static void MarkShotResult(PlayerInfoModel player, string row, int column, bool isAHit)
         {
-            throw new NotImplementedException();
+            foreach (var gridSpot in player.ShipLocations)
+            {
+                /* check for each row and column if it is already taken */
+                if (gridSpot.SpotLetter == row.ToUpper() && gridSpot.SpotNumber == column)
+                {
+                    if(isAHit)
+                    {
+                        gridSpot.Status = GridSpotStatus.Hit;
+                    }else
+                    {
+                        gridSpot.Status = GridSpotStatus.Miss;
+                    }
+                }
+            }
         }
     }
 }
