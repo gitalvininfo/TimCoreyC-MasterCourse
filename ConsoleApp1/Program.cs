917f46a256970eeaa3a9ee5b5b34154f52e0c1dc
Alvin Yanson <yansonalvin29@gmail.com>
2022-04-02 11:19:32 +0800

    done in abstract homework

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,25 +5,57 @@
         static void Main(string[] args)
         {
 
-            Car car = new Car();
-            Console.WriteLine(car.NumberOfWheels);
-            Console.ReadLine();
+            IVehicle vehicle = new Car();
+            vehicle.Start();
+            vehicle.Stop();
+            vehicle.Gas();
+            vehicle.Manufacturer = "Ferrari";
+            Console.WriteLine(vehicle.Manufacturer);
 
         }
 
     }
 
-    public abstract class Vehicle
+    public abstract class Vehicle : IVehicle
     {
         public string VIN { get; set; }
         public string Manufacturer { get; set; }
         public string Model { get; set; }
         public int YearManufactured { get; set; }
+
+        public void Gas()
+        {
+            Console.WriteLine("Gas");
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Start");
+
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stop");
+
+        }
     }
 
     public class Car : Vehicle
     {
         public int NumberOfWheels { get; set; } = 4;
+    }
+
+    public interface IVehicle
+    {
+        string VIN { get; set; }
+        string Manufacturer { get; set; }
+        string Model { get; set; }
+        int YearManufactured { get; set; }
+
+        void Start();
+        void Stop();
+        void Gas();
     }
 
 
