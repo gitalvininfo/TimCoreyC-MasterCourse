b62289953c3e8067176d346a874aca581b4de4ca
Alvin Yanson <yansonalvin29@gmail.com>
2022-04-15 11:26:07 +0800

    done in mini project

--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -7,34 +7,57 @@
 
             Person person = new Person();
 
-            person.SetDefaultAge().PrintInfo();
+            person.FirstName = "What is your first name: ".RequestString();
+            person.LastName = "What is your last name: ".RequestString();
+            person.Age = "What is your age: ".RequestInt();
+
+
+            Console.WriteLine(person.FirstName);
                         
             Console.ReadLine();
 
         }
     }
+
+    public static class ConsoleHelper
+    {
+        public static string RequestString(this string message)
+        {
+            string output = "";
+
+            while(string.IsNullOrWhiteSpace(output))
+            {
+                Console.Write(message);
+                output = Console.ReadLine();
+            }
+
+            return output;
+        }
+
+        public static int RequestInt(this string message)
+        {
+            int output = 0;
+            bool isValidInt = false;
+
+            while (!isValidInt)
+            {
+                Console.Write(message);
+                isValidInt = int.TryParse(Console.ReadLine(), out output);
+            }
+
+            return output;
+            
+        }
+    } 
+
     public class Person
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public int Age { get; set; }
-
-        public Person()
-        {
-        }
+        public int NumberOfChildren { get; set; }
     }
 
-    public static class ExtensionSample
-    {
-        public static Person SetDefaultAge(this Person person)
-        {
-            person.Age = 18;
-            return person;
-        }
-
-        public static void PrintInfo(this Person person)
-        {
-            Console.WriteLine($"{person.Age}");
-        }
-    }
 
 
 
